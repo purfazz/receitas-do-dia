976d9aa00a1453536f551a7d14d8d38affc9f54e
João Porfírio <porfiriojoao85@gmail.com>
2025-02-14 17:15:17 +0000

    feat: Adiciona Google Analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,23 @@
 import { ChakraProvider } from '@chakra-ui/react'
 import { Analytics } from '@vercel/analytics/react'
+import Script from 'next/script'
 
 export default function App({ Component, pageProps }) {
   return (
     <ChakraProvider>
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-DFLX0F5RND"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-DFLX0F5RND');
+        `}
+      </Script>
       <Component {...pageProps} />
       <Analytics />
     </ChakraProvider>
