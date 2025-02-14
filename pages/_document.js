eb04c2a698626bdf372c06ee2cfbc462177cef60
João Porfírio <porfiriojoao85@gmail.com>
2025-02-14 18:39:19 +0000

    feat: Adiciona otimizações adicionais de SEO e descoberta

--- pages/_document.js
+++ pages/_document.js
@@ -8,19 +8,26 @@ export default function Document() {
         <meta charSet="utf-8" />
         <meta name="theme-color" content="#319795" />
         
+        {/* Meta tags de idioma e região */}
+        <meta property="og:locale" content="pt_BR" />
+        <meta httpEquiv="content-language" content="pt-BR" />
+        <link rel="alternate" href="https://receitas-git-main-purfazzs-projects.vercel.app" hrefLang="pt-BR" />
+        <link rel="alternate" href="https://receitas-git-main-purfazzs-projects.vercel.app" hrefLang="x-default" />
+        
         {/* Favicons */}
         <link rel="icon" href="/favicon.ico" />
         <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
         <link rel="manifest" href="/site.webmanifest" />
         
         {/* Meta tags para SEO */}
-        <meta name="robots" content="index, follow" />
+        <meta name="robots" content="index, follow, max-image-preview:large" />
         <meta name="author" content="Receita do Dia" />
         <meta name="copyright" content="© 2024 Receita do Dia" />
+        <meta name="format-detection" content="telephone=no" />
         
         {/* Open Graph padrão */}
         <meta property="og:site_name" content="Receita do Dia" />
-        <meta property="og:locale" content="pt_BR" />
+        <meta property="og:type" content="website" />
         
         {/* Twitter Cards padrão */}
         <meta name="twitter:card" content="summary_large_image" />
@@ -30,6 +37,11 @@ export default function Document() {
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link rel="preconnect" href="https://www.google-analytics.com" />
+        <link rel="preconnect" href="https://pagead2.googlesyndication.com" />
+        
+        {/* DNS Prefetch */}
+        <link rel="dns-prefetch" href="https://www.google-analytics.com" />
+        <link rel="dns-prefetch" href="https://pagead2.googlesyndication.com" />
       </Head>
       
       {/* Google Analytics com consentimento padrão negado */}
@@ -44,7 +56,8 @@ export default function Document() {
               'ad_storage': 'denied'
             });
             gtag('config', 'G-DFLX0F5RND', {
-              'anonymize_ip': true
+              'anonymize_ip': true,
+              'cookie_flags': 'SameSite=None;Secure'
             });
           `
         }}
