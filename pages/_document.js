f06a9663b97718864ebf1f632b375db0908237e3
João Porfírio <porfiriojoao85@gmail.com>
2025-02-14 17:16:43 +0000

    fix: Corrige implementação do Google Analytics

---
+++ pages/_document.js
@@ -0,0 +1,29 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function Document() {
+  return (
+    <Html lang="pt">
+      <Head>
+        {/* Google Analytics */}
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-DFLX0F5RND"
+        />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-DFLX0F5RND');
+            `,
+          }}
+        />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+} 
